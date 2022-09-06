79f42370d1184d37a5bce93ca2f0024687b26ed2
Cami <camilacassimiro@gmail.com>
2022-09-06 16:37:15 -0300

    feature: mapeamento feito e models adaptadas

---
+++ src/Escola.Infra/DataBase/Mappings/Boletim.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Escola.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Escola.Infra.DataBase.Mappings
+{
+  public class BoletimMap : IEntityTypeConfiguration<Boletim>
+  {
+    public void Configure(EntityTypeBuilder<Boletim> builder)
+    {
+      builder.ToTable("Boletim");
+
+      builder.HasKey(x => x.Id);
+
+      builder.Property(x => x.Id)
+              .UseIdentityColumn()
+              .ValueGeneratedOnAdd();
+
+      builder.HasOne(x => x.Aluno)
+              .WithMany(x => x.Boletins)
+              .HasForeignKey(x => x.AlunoId);
+
+      builder.HasMany(x => x.Notas)
+              .WithOne(x => x.Boletim);
+    }
+  }
+}
