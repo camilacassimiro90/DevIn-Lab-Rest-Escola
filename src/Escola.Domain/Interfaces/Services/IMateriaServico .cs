e018c9c30b74f400c066bb825e4fb332c047de5e
Cami <camilacassimiro@gmail.com>
2022-09-09 15:55:56 -0300

    interface de materia criada

---
+++ src/Escola.Domain/Interfaces/Services/IMateriaServico .cs
@@ -0,0 +1,23 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Escola.Domain.DTO;
+using Escola.Domain.Interfaces.Services;
+using Escola.Domain.Interfaces.Repositories;
+using Escola.Domain.Models;
+using Escola.Domain.Exceptions;
+
+namespace Escola.Domain.Services
+{
+
+  public class MateriaServico : IMateriaServico
+  {
+    private readonly IMateriaRepositorio _materiaRepositorio;
+
+
+
+  }
+
+}
