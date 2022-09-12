26c6a5c0a21deecf6c6a4d7d05e13317f33eccae
Cami <camilacassimiro@gmail.com>
2022-09-12 14:01:44 -0300

    feature: validações de materias feitas

--- src/Escola.Domain/Models/Materia.cs
+++ src/Escola.Domain/Models/Materia.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Escola.Domain.DTO;
 
 namespace Escola.Domain.Models
 {
@@ -11,5 +12,21 @@ namespace Escola.Domain.Models
     public Guid Id { get; set; } = new Guid();
     public string Nome { get; set; }
     public virtual List<NotasMateria> NotasMaterias { get; set; }
+
+
+    public Materia()
+    {
+
+    }
+
+    public Materia(MateriaDTO materia)
+    {
+      Id = materia.Id;
+      Nome = materia.Nome;
+    }
+    public void Update(MateriaDTO materia)
+    {
+      Nome = materia.Nome;
+    }
   }
 }
--- src/Escola.Domain/Services/MateriaServico.cs
+++ src/Escola.Domain/Services/MateriaServico.cs
@@ -27,6 +27,36 @@ namespace Escola.Domain.Services
       _materiaRepositorio.Excluir(materia);
     }
 
-  }
+    public void Inserir(MateriaDTO materia)
+    {
+      var jaExiste = _materiaRepositorio.ObterPorNome(materia.Nome);
 
+      if (jaExiste.Count > 0)
+        throw new DuplicadoException("Matéria já existe");
+
+      _materiaRepositorio.Inserir(new Materia(materia));
+    }
+
+    public MateriaDTO ObterPorId(int id)
+    {
+      return new MateriaDTO(_materiaRepositorio.ObterPorId(id));
+    }
+
+    public List<MateriaDTO> ObterPorNome(string nome)
+    {
+      var jaExiste = _materiaRepositorio.ObterPorNome(nome);
+
+      if (jaExiste.Count > 0)
+        throw new DuplicadoException("Matéria já existe");
+      return _materiaRepositorio.ObterPorNome(nome).Select(x => new MateriaDTO(x)).ToList();
+
+    }
+
+    public IList<MateriaDTO> ObterTodos()
+    {
+      return _materiaRepositorio.ObterTodos()
+                                .Select(x => new MateriaDTO(x)).ToList();
+
+    }
+  }
 }
